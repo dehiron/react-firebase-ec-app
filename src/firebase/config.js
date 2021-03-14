13b0fad98de36b55dd1ade60793b02466322526b
Hidehiro Aya <hi.dehiro424@gmail.com>
2021-03-14 21:16:53 +0900

    firebase authentication実装の準備:config.jsとindex.jsを作成

---
+++ src/firebase/config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyB78IicBQWoHfEXWE9XsKSvul3-_xaT10o",
+    authDomain: "react-firebase-ec-app.firebaseapp.com",
+    projectId: "react-firebase-ec-app",
+    storageBucket: "react-firebase-ec-app.appspot.com",
+    messagingSenderId: "605053302970",
+    appId: "1:605053302970:web:0fb4dddf8eb38008242a83",
+    measurementId: "G-MSXNXLMLT9"
+};
+
+export {firebaseConfig};
---
+++ src/firebase/index.js
@@ -0,0 +1,15 @@
+import firebase from 'firebase/app'
+import 'firebase/auth'
+import 'firebase/firestore'
+import 'firebase/storage'
+import 'firebase/functions'
+import { firebaseConfig } from "./config";
+
+firebase.initializeApp(firebaseConfig);
+const auth = firebase.auth();
+const db = firebase.firestore();
+const storage = firebase.storage();
+const functions = firebase.functions();
+const FirebaseTimestamp = firebase.firestore.Timestamp;
+
+export {auth, db, storage, functions, FirebaseTimestamp};
