4c88859565620a45df8715e4fa1fea2307660c5c
Hidehiro Aya <hi.dehiro424@gmail.com>
2021-03-15 16:16:50 +0900

    タイポミス修正

--- src/components/UIkit/SelectBox.js
+++ src/components/UIkit/SelectBox.js
@@ -2,7 +2,7 @@ import React from 'react'
 import InputLabel from '@material-ui/core/InputLabel';
 import MenuItem from '@material-ui/core/MenuItem';
 import FormControl from '@material-ui/core/FormControl';
-import Select from '@material-ui/core/select'
+import Select from '@material-ui/core/Select'
 import { makeStyles } from '@material-ui/styles'
 
 const useStyles = makeStyles({
