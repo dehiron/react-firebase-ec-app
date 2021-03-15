91cebdb8e7effa71949964f5295694ae8e1f7cb4
Hidehiro Aya <hi.dehiro424@gmail.com>
2021-03-16 02:31:25 +0900

    デプロイ用設定ファイルの修正

--- src/Router.js
+++ src/Router.js
@@ -5,8 +5,8 @@ import { Home, SignUp, SignIn, Auth, Reset, ProductEdit} from "./templates";
 const Router = () => {
     return (
         <Switch>
-            <Route exact path="/signup" component={SignUp} />
             <Route exact path="/signin" component={SignIn} />
+            <Route exact path="/signup" component={SignUp} />
             <Route exact path="/signin/reset" component={Reset} />
             
             
