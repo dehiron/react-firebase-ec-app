8a73c1b9aa89ae3256dc1b661f19399236f3d7bc
Hidehiro Aya <hi.dehiro424@gmail.com>
2021-03-15 14:30:52 +0900

    Login.jsファイルの削除

--- src/templates/index.js
+++ src/templates/index.js
@@ -1,6 +1,5 @@
 export {default as Auth} from './Auth'
 export {default as Home} from './Home'
-export {default as Login} from './Login'
 export {default as SignUp} from './SignUp'
 export {default as SignIn} from './SignIn'
 export {default as Reset} from './Reset'
