4356e963f66bab3510e8f441f04df6cfac688ebc
Hidehiro Aya <hi.dehiro424@gmail.com>
2021-04-26 14:40:58 +0900

    redux-loggerの導入

--- src/reducks/store/store.js
+++ src/reducks/store/store.js
@@ -1,11 +1,24 @@
 import { createStore as reduxCreateStore, combineReducers, applyMiddleware } from 'redux'
 import thunk from 'redux-thunk';
+import { createLogger } from "redux-logger";
 //Reducersのインポート
 import { connectRouter, routerMiddleware } from 'connected-react-router';
 import　{ ProductsReducer } from '../products/reducers';
 import { UsersReducer } from '../users/reducers';
 
 function createStore(history){
+
+
+    const middleWares = [routerMiddleware(history), thunk]
+
+    if (process.env.NODE_ENV === "development") {
+        const logger = createLogger({
+            collapsed: true,
+            diff: true
+        })
+        middleWares.push(logger)
+    }
+
     return reduxCreateStore(
         combineReducers({
             // products: ProductReducer,
@@ -13,10 +26,7 @@ function createStore(history){
             products: ProductsReducer,
             router: connectRouter(history),
         }),
-        applyMiddleware(
-            routerMiddleware(history),
-            thunk
-        )
+        applyMiddleware(...middleWares)
     );
 }
 
